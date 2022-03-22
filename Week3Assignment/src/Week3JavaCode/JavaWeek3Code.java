b2829bc94628b4b4c6bc73a543be9f7abc24f9e8
Jacob Magliaro <jacob.magliaro@gmail.com>
2022-03-22 17:10:30 -0400

    first commit

---
+++ Week3Assignment/src/Week3JavaCode/JavaWeek3Code.java
@@ -0,0 +1,173 @@
+package Week3JavaCode;
+
+public class JavaWeek3Code {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	////-------------------------------1a----------------------------------
+
+	//	
+//					int[] age = {3, 9, 23, 64, 2, 8, 28, 93, 36};
+//					int length = age.length;
+//							age[age.length - 1] = age[age.length - 1] - age[0];
+//							for (int i = 0; i < age.length; i++) {
+//								System.out.println(age[i]);
+//							}
+//							
+//			////-------------------------------1b----------------------------------
+	//	
+//							int total = 0;
+//							for (int j = 0; j < length; j++) {
+//							total = total + age[j];
+//							}
+//							double average = total / length;
+//								System.out.println(average);
+	//
+//			////-------------------------------2a----------------------------------
+//												
+//							String[] names = {"Sam", "Tommy", "Tim", "Sally", "Buck", "Bob" };
+//							double counter = 0;
+//							for (String name : names) {
+//								counter = counter + name.length();
+//							}
+//								double namesAvg = counter / names.length;
+////								System.out.println(namesAvg);
+//					
+//			////-------------------------------2b----------------------------------
+//			
+//							String conNames = "";
+//									for (String name : names) {
+//									conNames = conNames + " " + name;
+//							}
+////									System.out.println(conNames);	
+////									
+							
+				
+			//-------------------------------3----------------------------------
+									
+									
+//			System.out.println(array[array.length - 1]);
+	//
+	//
+//			//-------------------------------4----------------------------------
+	//
+//									
+//			System.out.println(array[0]);
+				
+									
+			//-------------------------------5----------------------------------
+
+						
+					
+//					int[] nameLength = {names.length};
+//					for (String name : names) {
+//						int nameLen = name.length();
+//					nameLength[nameLength.length] = nameLen;
+//					}
+					
+			//------------------------------6----------------------------------
+						
+			
+				}
+			//-------------------------------7----------------------------------
+				
+//				
+//					public static String concatWords(String word, int n) {
+//						String concatWord = "" ;
+//						for (int f = 0; f <= n; f++ ) {
+//			            concatWord += word;
+//					}
+//						return concatWord;
+//					
+//				}
+//				
+//				
+//			//-------------------------------8----------------------------------
+	//
+//			        
+//			      public static String returnFullName(String firstName, String lastName) {
+//						String fullName = firstName + " " + lastName;
+//						return fullName;
+//			      }	
+//						
+//			//-------------------------------9----------------------------------
+//				
+//				
+//					public static boolean sumOfInts (int[] array) {
+//					int total3 = 0;
+//					for (int y = 0; y < array.length; y++) {
+//					total3 = total3 + array[y];
+//					}
+//						
+//				
+//				return total3 > 100;
+//				
+//					}
+						
+
+//			-------------------------------10----------------------------------
+				
+				
+//					public static double returnAvg(double[] array) {
+//						double fullAvg = 0;
+//						for (int d = 0; d < array.length; d++) {
+//						fullAvg = fullAvg + array[d];
+//						}
+//						double finalAvg = fullAvg / array.length;
+//						return finalAvg;
+//					
+//					}
+//				
+//			//-------------------------------11----------------------------------
+	//
+//				
+//					public static boolean avgOfElements(double[] array, double [] array2)  {
+//						//array
+//							double arrayAvg1 = 0;
+//							for (int w = 0; w < array.length; w++) {
+//							arrayAvg1 = arrayAvg1 + array[w];
+//							}
+//							double avg1 = arrayAvg1 / array.length;
+//							
+//						//array2		
+//							double arrayAvg2 = 0;
+//							for (int n = 0; n < array2.length; n++) {
+//							arrayAvg2 = arrayAvg2 + array2[n];
+//							}
+//							double avg2 = arrayAvg2 / array2.length;
+//						//return
+//							return avg1 > avg2;
+//				
+//			}
+//			//-------------------------------12----------------------------------
+	//
+//				
+//					public static boolean willBuyDrink(boolean isHotOutside, double moneyInPocket) {
+//						return ((moneyInPocket > 10.50) && (isHotOutside == true));		
+//							}
+				
+		
+			//-------------------------------13----------------------------------
+		//Write a method that uses an array of grades to find whether the student is getting a passing grade (at least a 70) 
+		//if average is more than 95, have it say "doing great!"
+//			public static String passingGrade(double[] grade) {
+//				double gradeAvg = 0;
+//				for (int w = 0; w < grade.length; w++) {
+//				gradeAvg = gradeAvg + grade[w];
+//				}
+//				double average = gradeAvg / grade.length;
+//				
+//				if (average >= 70 && average < 95) {
+//					return "passing";
+//				}else if (average <= 100 && average >= 95) {
+//					return "passing, doing great!";
+//				}else{
+//					return "failing";
+//				}
+//			}
+	
+		
+
+	}
+
+
